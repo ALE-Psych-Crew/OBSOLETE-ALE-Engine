fa01960ad4bad20c8d0cd88b2be3ad24feb8821b
Alejo GD Official <102831994+AlejoGDOfficial@users.noreply.github.com>
2025-05-31 22:25:48 -0500

    setStrumLineProperty & setStrumProperty

--- source/scripting/lua/LuaPlayState.hx
+++ source/scripting/lua/LuaPlayState.hx
@@ -8,27 +8,76 @@ import funkin.visuals.game.*;
 
 class LuaPlayState extends LuaPresetBase
 {
+    var game:PlayState = PlayState.instance;
+
     override public function new(lua:LuaScript)
     {
         super(lua);
 
-        var game:PlayState = PlayState.instance;
-
         set('setCharacterProperty', function(type:ALECharacterType, index:Int, props:Dynamic)
             {
-                var object:Character = switch(type)
-                {
-                    case PLAYER:
-                        game.characters.players.members[index];
-                    case OPPONENT:
-                        game.characters.opponents.members[index];
-                    case EXTRA:
-                        game.characters.extras.members[index];
-                }
+                var object:Character = getCharacter(type, index);
 
                 if (object != null)
                     LuaReflect.applyProps(object, props);
             }
         );
+
+        set('setStrumLineProperty', function(type:ALECharacterType, index:Int, props:Dynamic)
+            {
+                var object:StrumLine = getStrumLine(type, index);
+
+                if (object != null)
+                    LuaReflect.applyProps(object, props);
+            }
+        );
+
+        set('setStrumProperty', function(type:ALECharacterType, groupIndex:Int, index:Int, props:Dynamic)
+            {
+                var object:Strum = getStrum(type, groupIndex, index);
+
+                if (object != null)
+                    LuaReflect.applyProps(object, props);
+            }
+        );
+    }
+
+    function getCharacter(type:ALECharacterType, index:Int):Character
+    {
+        return switch(type)
+            {
+                case PLAYER:
+                    game.characters.players.members[index];
+                case OPPONENT:
+                    game.characters.opponents.members[index];
+                case EXTRA:
+                    game.characters.extras.members[index];
+            }
+    }
+
+    function getStrumLine(type:ALECharacterType, index:Int):StrumLine
+    {
+        return switch(type)
+            {
+                case PLAYER:
+                    game.strumLines.players.members[index];
+                case OPPONENT:
+                    game.strumLines.opponents.members[index];
+                case EXTRA:
+                    game.strumLines.extras.members[index];
+            }
+    }
+
+    function getStrum(type:ALECharacterType, groupIndex:Int, index:Int):Strum
+    {
+        return switch(type)
+            {
+                case PLAYER:
+                    game.strumLines.players.members[groupIndex].strums.members[index];
+                case OPPONENT:
+                    game.strumLines.opponents.members[groupIndex].strums.members[index];
+                case EXTRA:
+                    game.strumLines.extras.members[groupIndex].strums.members[index];
+            }
     }
 }
