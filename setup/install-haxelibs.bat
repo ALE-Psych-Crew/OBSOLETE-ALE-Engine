626d8a76b08ad6e6f7b56abd0df40f6da5babc8d
AlejoGDOfficial <alejogdoff@gmail.com>
2025-06-17 19:37:01 -0500

    da lib

--- setup/install-haxelibs.bat
+++ setup/install-haxelibs.bat
@@ -11,7 +11,7 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc05139
 haxelib install away3d 5.0.9
 haxelib install openfl 9.4.1
 haxelib install tjson 1.4.0
-haxelib git sl-windows-api https://github.com/Slushi-Github/SL-Windows-API main
+haxelib install sl-windows-api 1.1.0
 haxelib install lime 8.2.2
 haxelib git flixel 6.1.0
 haxelib install flixel-addons 3.3.2
