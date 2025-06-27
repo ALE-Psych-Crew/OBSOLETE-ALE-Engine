674789eb314a95199fdb68c2f70f2d77e6df2097
AlejoGDOfficial <alejogdoff@gmail.com>
2025-06-27 14:42:16 -0500

    haxe is retarded

--- setup/install-haxelibs.bat
+++ setup/install-haxelibs.bat
@@ -10,6 +10,7 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc05139
 haxelib git SScript https://github.com/AlejoGDOfficial/SScript-7.7.0
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 1966f8fbbbc509ed90d4b520f3c49c084fc92fd6
+haxelib git ale-ui https://github.com/ALE-Engine-Crew/ALE-UI --skip-dependencies
 
 haxelib install away3d 5.0.9
 haxelib install openfl 9.4.1
@@ -17,9 +18,8 @@ haxelib install tink_core 1.26.0
 haxelib install tjson 1.4.0
 haxelib install sl-windows-api 1.1.0
 haxelib install lime 8.2.2
-haxelib git flixel 6.1.0
+haxelib install flixel 6.1.0
 haxelib install flixel-addons 3.3.2
 haxelib install extension-androidtools 2.2.1 --skip-dependencies
 haxelib install hxdiscord_rpc 1.3.0 --skip-dependencies
-haxelib git ale-ui https://github.com/ALE-Engine-Crew/ALE-UI --skip-dependencies
 haxelib install hxvlc 2.2.2 --skip-dependencies
